3b42c5d222a96cb37ea2f97f537a49af8ae36865
Yash Thakkar <thakkaryash94@gmail.com>
2020-08-07 08:03:05 +0530

    feat: Add CloudFront config

--- deployment/main.ts
+++ deployment/main.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { App, TerraformStack, TerraformOutput } from 'cdktf';
 import * as glob from 'glob';
 import * as mime from 'mime-types';
-import { AwsProvider, S3Bucket, S3BucketObject } from './.gen/providers/aws';
+import { AwsProvider, S3Bucket, S3BucketObject, CloudfrontDistribution, CloudfrontOriginAccessIdentity } from './.gen/providers/aws';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -62,9 +62,90 @@ class MyStack extends TerraformStack {
       });
     }
 
+    const originId = `S3-${BUCKET_NAME}`;
+
+    /*
+     * Create am Origin Access Identity
+     * Doc link: https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-access-to-amazon-s3/
+     * Tutorial link: https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-access-to-amazon-s3/
+     */
+    const cloudfrontOriginAccessIdentity = new CloudfrontOriginAccessIdentity(this, 'aws_cloudfront_origin_access_identity', {
+      comment: 's3-cloudfront-cdk-example'
+    })
+
+    const cloudFrontDistribution = new CloudfrontDistribution(this, `aws_cloudfront_${BUCKET_NAME}`, {
+      enabled: true,
+      dependsOn: [bucket],
+      defaultRootObject: 'index.html',
+      customErrorResponse: [{
+        errorCode: 404,
+        responseCode: 200,
+        responsePagePath: '/index.html'
+      }],
+      origin: [{
+        originId: originId,
+        domainName: bucket.bucketDomainName,
+        s3OriginConfig: [{
+          originAccessIdentity: cloudfrontOriginAccessIdentity.cloudfrontAccessIdentityPath
+        }]
+      }],
+      defaultCacheBehavior: [{
+        allowedMethods: ['GET', 'HEAD'],
+        cachedMethods: ['GET', 'HEAD'],
+        forwardedValues: [{
+          cookies: [{ forward: 'none' }],
+          queryString: false
+        }],
+        targetOriginId: originId,
+        viewerProtocolPolicy: 'allow-all'
+      }],
+      restrictions: [{
+        geoRestriction: [{
+          restrictionType: 'none'
+        }]
+      }],
+      viewerCertificate: [{
+        cloudfrontDefaultCertificate: true
+      }],
+    });
+
+    /*
+     * Previously, our bucket was public, it means anyone can access bucket objects using bucket website URL.
+     * Now, we have configured CloudFront to serve our website, so it's time to block that access.
+     * With this, our website will be only accessible by CloudFront URL only.
+     * No-one will be able to access bucket objects using S3 website URL.
+    */
+    bucket.acl = 'private'
+    bucket.website = []
+    bucket.policy = `{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${cloudfrontOriginAccessIdentity.id}"
+          },
+          "Action": [
+            "s3:GetObject"
+          ],
+          "Resource": [
+            "arn:aws:s3:::${BUCKET_NAME}/*"
+          ]
+        }
+      ]
+    }`
+
     // Output the bucket url to access the website
     new TerraformOutput(this, 'website_endpoint', {
+      description: 'S3 Bucket Public URL',
       value: `http://${bucket.websiteEndpoint}`
+    });
+
+    // Output the cloudfront url to access the website
+    new TerraformOutput(this, 'cloudfront_website_endpoint', {
+      description: 'CloudFront URL',
+      value: `https://${cloudFrontDistribution.domainName}`
     });
 
   }
